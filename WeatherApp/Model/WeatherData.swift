a374327913276fad9f2e1d42128707d96ce63386
Prachi Bile <bileprachi@gmail.com>
2021-06-01 11:14:46 +0530

    Replaced Coddle with Decodable as my struct is only used to decode

--- WeatherApp/Model/WeatherData.swift
+++ WeatherApp/Model/WeatherData.swift
@@ -7,17 +7,17 @@
 
 import Foundation
 
-struct WeatherData: Codable {
+struct WeatherData: Decodable {
     let name: String
     let main: Main
     let weather: [Weather]
 }
 
-struct Main: Codable {
+struct Main: Decodable {
     let temp: Double
 }
 
-struct Weather: Codable {
+struct Weather: Decodable {
     let main: String
     let description: String
     let icon: String
